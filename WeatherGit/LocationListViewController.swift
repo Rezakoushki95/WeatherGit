9dcc00220ebf2bd294e7aa3c282a0cc8d8f93e3e
rezakoushki95 <rezaaarhus@hotmail.com>
2023-01-11 18:31:57 +0100

    - Freeze the top cell (user's own location), so it can't be moved, edited or deleted.

--- WeatherGit/LocationListViewController.swift
+++ WeatherGit/LocationListViewController.swift
@@ -85,6 +85,20 @@ extension LocationListViewController: UITableViewDelegate, UITableViewDataSource
 		return cell
 	}
 	
+	// MARK: TableView methods to freeze the first cell
+	
+	func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+		return indexPath.row != 0 ? true : false
+	}
+	
+	func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+		return indexPath.row != 0 ? true : false
+	}
+	
+	func tableView(_ tableView: UITableView, targetIndexPathForMoveFromRowAt sourceIndexPath: IndexPath, toProposedIndexPath proposedDestinationIndexPath: IndexPath) -> IndexPath {
+		return (proposedDestinationIndexPath.row == 0 ? sourceIndexPath : proposedDestinationIndexPath)
+	}
+	
 	
 }
 
